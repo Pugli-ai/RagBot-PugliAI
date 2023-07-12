da896c881b23d0ee24c0a3f2a1c59704e1db44e3
Kerem Tatlıcı <33085629+keremtatlici@users.noreply.github.com>
2023-07-12 14:09:59 +0200

    qa run, qa scraper added and post request added

---
+++ api/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Query
+from api import qa_run
+import pandas as pd
+import numpy as np
+from pydantic import BaseModel
+
+app = FastAPI()
+df=pd.read_csv('processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+#examples
+@app.get("/api")
+async def genereteResponse():
+    pass
+
+@app.get("/api/{id}")
+def home(id: int):
+    return {"id": id}
+
+
+class Question(BaseModel):
+    question: str
+
+@app.post("/qa")
+def generate_response(question: Question):
+    answer = qa_run.answer_question(df, question=question.question, debug=False)
+    print(answer)
+    return answer
+
+"""
+@app.get("/qa")
+def generate_response(question: str = Query(..., min_length=1)):
+    answer = qa_run.answer_question(df, question=question, debug=False)
+    print(answer)
+    return answer
+
+#http://localhost:8000/qa?question=How%20to%20connect%20Tiledesk%20with%20Telegram
+"""
