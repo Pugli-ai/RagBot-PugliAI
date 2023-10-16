04177b993a8e43098c975f01d8ac0ba29be8ca24
Kerem Tatlıcı <33085629+keremtatlici@users.noreply.github.com>
2023-10-16 10:36:36 +0200

    try

--- backend/api/main.py
+++ backend/api/main.py
@@ -45,7 +45,7 @@ class Status_Inputs(BaseModel):
 
 @app.post("/api/qa")
 def qa_run_api(inputs: QA_Inputs):
-    print("API called#####################################")
+    print("Hello , its a qa #####################################")
     answer = qa_run.main(
         question=inputs.question,
         openai_api_key=inputs.gptkey,
