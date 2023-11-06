2ff03316345b0703e53bb7c581813d7e112bb071
Kerem Tatlıcı <33085629+keremtatlici@users.noreply.github.com>
2023-11-06 19:27:03 +0100

    langchain output parser integration done

--- backend/api/qa_run.py
+++ backend/api/qa_run.py
@@ -16,6 +16,11 @@ import json
 from datetime import datetime
 import time
 
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
+
 context_print_option= False
 
 ########################################################### CHILD FUNCTIONS ###########################################################
@@ -107,6 +112,7 @@ def conversation(context: str, question: str, chat_history: str = "", model: str
 
     return response['choices'][0]['message']['content']
 
+
 def conversation_with_langchain(context: str, question: str, chat_history: str = "", model: str = "gpt-3.5-turbo", max_tokens: int = 1500) -> str:
     """
     Generate a conversation based on the provided context and question.
@@ -118,88 +124,62 @@ def conversation_with_langchain(context: str, question: str, chat_history: str =
         max_tokens (int, optional): Maximum tokens for the response. Defaults to 1500.
 
     Returns:
-        str: The model's response.
+        dict: The model's response, as dictionary.
     """
 
-    template = """# Chatbot Instructions
+    template_string = """# Chatbot Instructions
     
     You are a chatbot programmed to provide precise answers to questions by exploring a given webpage and its subpages. Your objective is to parse through the provided context to find the most accurate answer to the question asked. Follow these guidelines to ensure correct behavior:
 
     ## Guidelines
     - **Greeting**: Always respond to greeting messages with something like, 'Hi, how can I help you?'
     - **Language**: Ensure that your answer is in the same language as the question asked.
-    - **JSON Formatting**: Format your response as a JSON object with the root key labeled as 'Response'.
-
-        ```json
-        {{
-            'Response': {{
-                'questions_language': 'language',
-                'answer': 'answer in question\'s language',
-                'source_url': 'source_url'
-            }}
-        }}
-        ```
-
-    - **JSON Readability**: Make sure that the answer is readable using the `json.loads(answer)` Python code. If it's not, it won't be processed correctly.
     - **Source URL**: If the answer is found in the context, include its source URL. If it's not, set `source_url` to `None`.
 
     ---
 
-    Context: {context}
+    Context: ```{context}```
 
     ---
 
-    Chat History: {chat_history}
+    Chat History: ```{chat_history}```
 
     ---
 
-    Question: {question}"""
+    Question: ```{question}```
 
-
-    template_old = """
-    You are a chatbot seeking precise answers to given questions by exploring a webpage and its subpages. Your goal is to sift through the provided context to find the most accurate answer to the question asked. If the context contains a direct or related answer, provide it with question's language. If the answer is not present in the context or chat history say "I don't know".
-    These are the rules;
-    * Always make a respond to greetings messages you can say like "hi how can i help you?".
-    * Translate your answer to the question's language.
-    * If the answer in the context provide its source url. You will find it in the context. Otherwise make source url None.
-    ---
-
-    Context: {context}
-
-    ---
-
-    Chat History:
-    {chat_history}
-
-    ---
-
-    Question: {question}
-
-    {{"Response" :
-        {{
-        "questions_language" : "language",
-        "answer": "answer in question's language",
-        "source_url": "source_url"
-        }}
-    }}
+    format_instructions : ```{format_instructions}```
     """
-    
-    prompt = PromptTemplate(input_variables=["context", "chat_history", "question"], template=template)
-    LLM = ChatOpenAI(temperature=0, max_tokens=max_tokens, model=model)
-    chatgpt_chain = LLMChain(llm=LLM, prompt=prompt, verbose=False)
-    answer = chatgpt_chain.predict(context=context, chat_history=chat_history, question=question)
-    # print("########################################################")
-    # print("ANSWER: ")
-    # print(answer)
-    # print("########################################################")
-    try:
-        answer_json = json.loads(answer)
-        
-    except:
-        print("JSON IS BROKEN FOR THE FIRST REQUEST, NOW TRYING TO FIX IT WITH ANOTHER CHATGPT REQUEST")
-        answer_json = fix_json_with_langchain(answer, model=model, max_tokens=max_tokens)
 
-    return answer_json['Response']
+    LLM = ChatOpenAI(temperature=0, max_tokens=max_tokens, model=model, verbose=True)
+    # Define the response schemas for structured output
+    response_schemas = [
+        ResponseSchema(name="questions_language", description="The language of the question."),
+        ResponseSchema(name="answer", description="The answer to the question."),
+        ResponseSchema(name="source_url", description="The source URL of the answer.")
+    ]
+    
+    output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+    format_instructions = output_parser.get_format_instructions()
+
+    prompt = ChatPromptTemplate.from_template(template=template_string)
+
+    messages = prompt.format_messages(
+        context=context,
+        chat_history=chat_history,
+        question=question,
+        format_instructions=format_instructions)
+    
+    response = LLM(messages)
+
+    response_string = response.content.replace("\n", "")
+    # print("\n########################################################")
+    # print("ANSWER: ")
+    # print(response_string)
+    # print("########################################################\n")
+    parsed_response = output_parser.parse(response_string)
+    return parsed_response
+
 
 def fix_json_with_langchain(broken_json: str, model: str = "gpt-3.5-turbo", max_tokens: int = 1500) -> str:
     """ this method for fixing the broken json with langchain
@@ -364,8 +344,8 @@ init()
 
 if __name__ == "__main__":
 
-    full_url = "https://www.deghi.it/supporto/"
-    #full_url= "https://gethelp.tiledesk.com/"
+    #full_url = "https://www.deghi.it/supporto/"
+    full_url= "https://gethelp.tiledesk.com/"
     #full_url = "https://docs.pinecone.io/"
 
     #full_url = "https://knowledge.webafrica.co.za"
@@ -381,14 +361,58 @@ if __name__ == "__main__":
 
     if full_url == "https://gethelp.tiledesk.com/":
         question_list = [
-                        "how can I connect tiledesk to WhatsApp?",
-                        "What is tiledesk?",
-                        "What day is it?",
-                        "which javascript code should i copy and paste for installing the widget on my website ? please write me that javascript code",
-                        "Ciao",
-                        "How to connect Tiledesk with Telegram",
+        "How can I connect Tiledesk to WhatsApp?",
+        "What is Tiledesk?",
+        "How to integrate Tiledesk with my CRM?",
+        "Which JavaScript code should I use for installing the Tiledesk widget on my website?",
+        "How to automate responses in Tiledesk?",
+        "Can I use Tiledesk for managing multiple teams?",
+        "How to connect Tiledesk with Telegram?",
+        "What are the pricing plans for Tiledesk?",
+        "Is there a mobile app available for Tiledesk?",
+        "How do I customize the Tiledesk chat interface?",
+        "Can Tiledesk handle chatbots for customer service?",
+        "How to set up multilingual support in Tiledesk?",
+        "What analytics features does Tiledesk offer?",
+        "How can I import existing customer support tickets into Tiledesk?",
+        "What kind of APIs does Tiledesk offer for integration?",
+        "How secure is customer data in Tiledesk?",
+        "Can Tiledesk be used for video calls?",
+        "How does Tiledesk handle offline messages?",
+        "What are the options for customer feedback collection in Tiledesk?",
+        "How to add custom fields to Tiledesk contact forms?",
+        "How can I export reports from Tiledesk?",
+        "Which ports need to be open for Tiledesk's operation?",
+        "How to retrieve the full chat history via Tiledesk API?",
+        "What webhook events are available in Tiledesk?",
+        "How to handle file uploads through the Tiledesk widget?",
+        "What are the limitations of the free version of Tiledesk?",
+        "Good morning, what are the support hours for Tiledesk?",
+        "Hello, could you guide me through the setup process?",
+        "What day is it?",
+        "Ciao",
+        "How to manage agent permissions in Tiledesk?",
+        "Can Tiledesk integrate with social media platforms?",
+        "What are the best practices for setting up an effective knowledge base in Tiledesk?",
+        "What's the weather like today?",
+        "Can you recommend a good book to read?",
+        "What's the best way to learn a new language?",
+        "How do I bake a chocolate cake?",
+        "Are there any good movies playing this weekend?",
+        "What's the latest score of the football game?",
+        "How do I start learning yoga?",
+        "What are the seven wonders of the world?",
+        "Can you give me some tips for photography?",
+        "How do I reset my phone when it freezes?",
+        "What are some healthy dinner options?",
+        "Can you explain the theory of relativity?",
+        "What are the trending topics on social media today?"
+    ]
+        question_list_old = [
+            "How can I export reports from Tiledesk?",
+            "How to add custom fields to Tiledesk contact forms?"
 
-        ]
+    ]
         for question in question_list:
             answer = main(question, variables_db.OPENAI_API_KEY, full_url)
 
@@ -494,53 +518,81 @@ if __name__ == "__main__":
         ] 
         for question in question_list:
             answer = main(question, variables_db.OPENAI_API_KEY, full_url)
+
+
 '''
-Prompts,
-template = """
-You are an individual seeking precise answers to your questions by exploring a webpage and its subpages. Your goal is to sift through the provided context to find the most accurate answer to the question asked. If the context contains a direct answer, provide it. If the context contains a related but not exact answer, indicate that. If the answer is not present in the context or chat history, simply state "I don't know". Do not make guesses or generate unrelated responses. Respond in the same language as the question asked.
+def conversation_with_langchain(context: str, question: str, chat_history: str = "", model: str = "gpt-3.5-turbo", max_tokens: int = 1500) -> str:
+    """
+    Generate a conversation based on the provided context and question.
 
----
+    Args:
+        context (str): The context for the conversation.
+        question (str): The question to be answered.
+        model (str, optional): The model to be used for the conversation. Defaults to "gpt-4".
+        max_tokens (int, optional): Maximum tokens for the response. Defaults to 1500.
 
-Context: {context}
+    Returns:
+        str: The model's response.
+    """
 
----
+    template = """# Chatbot Instructions
+    
+    You are a chatbot programmed to provide precise answers to questions by exploring a given webpage and its subpages. Your objective is to parse through the provided context to find the most accurate answer to the question asked. Follow these guidelines to ensure correct behavior:
 
-Chat History:
-{chat_history}
+    ## Guidelines
+    - **Greeting**: Always respond to greeting messages with something like, 'Hi, how can I help you?'
+    - **Language**: Ensure that your answer is in the same language as the question asked.
+    - **JSON Formatting**: Format your response as a JSON object with the root key labeled as 'Response'.
 
----
+        ```json
+        {{
+            'Response': {{
+                'questions_language': 'language',
+                'answer': 'answer in question\'s language',
+                'source_url': 'source_url'
+            }}
+        }}
+        ```
 
-Question: {question}
-Answer:
-"""
+    - **JSON Readability**: Make sure that the answer is readable using the `json.loads(answer)` Python code. If it's not, it won't be processed correctly.
+    - **Source URL**: If the answer is found in the context, include its source URL. If it's not, set `source_url` to `None`.
 
+    ---
 
+    Context: {context}
 
+    ---
 
+    Chat History: {chat_history}
 
-template = """
-Answer the question based on the context below. If the answer isn't in the context, refer to the chat history. If the answer can't be found in either, say "I don't know".
+    ---
 
----
+    Question: {question}"""
+   
+    prompt = PromptTemplate(input_variables=["context", "chat_history", "question"], template=template)
+    LLM = ChatOpenAI(temperature=0, max_tokens=max_tokens, model=model)
+    chatgpt_chain = LLMChain(llm=LLM, prompt=prompt, verbose=False)
+    answer = chatgpt_chain.predict(context=context, chat_history=chat_history, question=question)
+    # print("########################################################")
+    # print("ANSWER: ")
+    # print(answer)
+    # print("########################################################")
+    try:
+        answer_json = json.loads(answer)
+        
+    except:
+        print("JSON IS BROKEN FOR THE FIRST REQUEST, NOW TRYING TO FIX IT WITH ANOTHER CHATGPT REQUEST")
+        answer_json = fix_json_with_langchain(answer, model=model, max_tokens=max_tokens)
 
-Context: {context}
+    return answer_json['Response']
 
----
-
-Chat History:
-{chat_history}
-
----
-
-Question: {question}
-Answer:
-"""
-
-
-
-    template = """
-    You are an individual seeking precise answers to your questions by exploring a webpage and its subpages. Your goal is to sift through the provided context to find the most accurate answer to the question asked. Also you are asking with your mother language, for example if the questions is in English than your mother language is English but you are able to understand all the languages. If the context contains a direct or related answer, provide it with your mother language. If the answer is not present in the context or chat history, say exactly "I don't know" noting else. Do not make guesses or generate unrelated responses.
-
+    
+    template_old = """
+    You are a chatbot seeking precise answers to given questions by exploring a webpage and its subpages. Your goal is to sift through the provided context to find the most accurate answer to the question asked. If the context contains a direct or related answer, provide it with question's language. If the answer is not present in the context or chat history say "I don't know".
+    These are the rules;
+    * Always make a respond to greetings messages you can say like "hi how can i help you?".
+    * Translate your answer to the question's language.
+    * If the answer in the context provide its source url. You will find it in the context. Otherwise make source url None.
     ---
 
     Context: {context}
@@ -553,6 +605,14 @@ Answer:
     ---
 
     Question: {question}
-    Answer:
+
+    {{"Response" :
+        {{
+        "questions_language" : "language",
+        "answer": "answer in question's language",
+        "source_url": "source_url"
+        }}
+    }}
     """
+
 '''
